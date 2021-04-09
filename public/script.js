0e80f05170ff9a0580018bf27d97e2079f147790
shrutik1999 <kshruti1999@gmail.com>
2021-04-09 00:15:05 -0400

    fixed innerhtml and table display

--- public/script.js
+++ public/script.js
@@ -1,15 +1,14 @@
-//const wholeMealData = document.querySelector('.wholeMealInfo');
+const wholeMealData = document.querySelector('.wholeMealInfo');
 
 function getRandomIntInclusive(min, max) {
   min = Math.ceil(min);
   max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min + 1) + min); //The maximum is inclusive and the minimum is inclusive
+  return Math.floor(Math.random() * (max - min + 1) + min); // The maximum is inclusive and the minimum is inclusive
 }
 async function getWholeMeals() {
-  const endpoint = '/api/wholeMeal'; //changed endpoint name
-  const request = await fetch(endpoint); //changed from endpoint
-  const json = await request.json(); //changed json to jsonmeals
-  //const mealsarray = json["data"];
+  const endpoint = '/api/wholeMeal'; // changed endpoint name
+  const request = await fetch(endpoint); // changed from endpoint
+  const json = await request.json(); // changed json to jsonmeals
   return json;
 }
 
@@ -17,25 +16,30 @@ async function windowActions() {
   /* set a constant, html, equal will take the array from the data and insert into tbody */
   console.log('loaded window');
   const results = await getWholeMeals();
+  console.log(results);
   const meals = results.data;
-  
+
   const mealArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
   const selectedMeals = mealArray.map((element) => {
     const random = getRandomIntInclusive(0, meals.length - 1);
     return meals[random];
   });
   console.table(selectedMeals);
-}
 
-  /*const html = mealsarray.map(place => {
+  const html = selectedMeals.map(i => {
     return `
         <tr>
-            <td class="hallid">${place.hall_id}</td>
-            <td class="hallname">${place.hall_name}</td>
-            <td class="halladdress">${place.hall_address}</td>
+            <td class="mealid">${i.meal_id}</td>
+            <td class="mealname">${i.meal_name}</td>
+            <td class="mealcalories">${i.calories}</td>
+            <td class="mealcarbs">${i.carbs}</td>
+            <td class="mealsodium">${i.sodium}</td>
+            <td class="mealprotein">${i.protein}</td>
+            <td class="mealfat">${i.fat}</td>
+            <td class="mealchol">${i.cholesterol}</td>
         </tr>
     `;}).join('');
     wholeMealData.innerHTML = html;
-}*/
+}
 
 window.onload = windowActions; /* calls windowActions as soon as page loads */
